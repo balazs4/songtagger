0f1f35788bf0fad3d90fdfec7e2d8404799abcd8
balazs4_cp <SND\balazs4_cp>
2012-09-29 15:58:51 +0000

    + uTest namespace refactoring + iTest project intro

---
+++ SongTagger/SongTagger.Core/Providers/MusicDataProvider.cs
@@ -0,0 +1,65 @@
+//
+//  MusicDataProvider.cs
+//
+//  Author:
+//       balazs4 <balazs4web@gmail.com>
+//
+//  Copyright (c) 2012 GPLv2 (CodePlex)
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+using System;
+using System.Collections.Generic;
+
+namespace SongTagger.Core
+{
+
+    public class MusicDataProvider : IProvider
+    {
+        #region Singleton pattern
+        private static MusicDataProvider singletonInstance;
+
+        public static IProvider Provider
+        {
+            get
+            {
+                if (singletonInstance == null)
+                    singletonInstance = new MusicDataProvider();
+
+                return singletonInstance as IProvider;
+            }
+        }
+
+        private MusicDataProvider()
+        {
+
+        }
+        #endregion
+
+        #region IProvider implementation
+        public IArtist GetArtist(string nameStub)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<IAlbum> GetReleases(IArtist artist, IEnumerable<ReleaseType> releaseTypeList)
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+
+    }
+}
+
